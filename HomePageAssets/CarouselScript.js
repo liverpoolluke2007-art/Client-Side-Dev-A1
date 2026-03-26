0a18394cafc8c3b79132135fcf619513c2a168be
Luke Wallace <259457382+liverpoolluke2007-art@users.noreply.github.com>
2026-03-26 22:59:07 +0000

    trying to get carousel to work again
    Refactored showSlides function to retrieve slides within the function scope.

--- HomePageAssets/CarouselScript.js
+++ HomePageAssets/CarouselScript.js
@@ -1,19 +1,21 @@
 let slideIndex = 0;
-const slides = document.getElementsByClassName("slide");
+showSlides(slideIndex);
 
 function changeSlide(n) {
     showSlides(slideIndex += n);
 }
 
 function showSlides(n) {
-    if (n >= slides.length) { slideIndex = 0; }
-    if (n < 0) { slideIndex = slides.length - 1; }
-
+    let slides = document.getElementsByClassName("slide");
+    
+    if (n >= slides.length) {slideIndex = 0}    
+    if (n < 0) {slideIndex = slides.length - 1}
+    
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "none";  
         slides[i].classList.remove("active");
     }
-
+    
     slides[slideIndex].style.display = "flex";
     slides[slideIndex].classList.add("active");
 }
